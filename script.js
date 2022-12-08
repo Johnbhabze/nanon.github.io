937006f09e974f8ef29ee6f41c9b11f41288d7fb
Johnbhabze <115083981+Johnbhabze@users.noreply.github.com>
2022-12-08 15:40:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+/* hamburger menu botton */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function media(){
+confirm("Thank you for visiting Nanon"+"\n"+"\n"+"You are about to visit a website outside of Nanon. Please familiarize yourself with this other website's Privacy Policy as it differs from ours.");
+} 
